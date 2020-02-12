c4601ad48bb87d5cd2d85bc4f26280556482a406
Mikhail <belousov.nvkz@mail.ru>
2020-02-12 19:26:10 +0700

    animation products

--- js/script.js
+++ js/script.js
@@ -15,3 +15,12 @@ $win.on("scroll", function() {
         tgl(false);
     }
 });
+
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
