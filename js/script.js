771241ef449ad918d19d7f121b1220180136a026
Mikhail <belousov.nvkz@mail.ru>
2020-02-17 22:09:39 +0700

    swipper 2

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 /* HEADER MENU */
 let win = $(window),
     fixed = $(".header_fixed"),
+    fixedLogo = $(".header__logo_grey"),
+    whiteLogo = $(".header__logo"),
     limit = 1080;
 
 function tgl (state) {
     fixed.toggleClass("header_hidden", state);
+    fixedLogo.toggleClass("header_hidden", state);
 }
 
 win.on("scroll", function() {
@@ -26,3 +29,21 @@ $(document).ready(function(){
         $('body,html').animate({scrollTop: top}, 1500);
     });
 });
+
+/* Swiper */
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+/* _______________________________________ */
+/* Swiper 2*/
+var mySwiper2 = new Swiper('#swiper-container2', {
+    watchSlidesProgress: true,
+    watchSlidesVisibility: true,
+    slidesPerView: 2.5
+})
+
+
